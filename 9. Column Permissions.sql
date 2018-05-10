5bac61a12f0d2cf5b4541e21cdf147fc81c997bc
techbb <techbb@FHLBDM.Com>
2018-05-10 11:31:46 -0500

    Last update at Work

---
+++ 9. Column Permissions.sql
@@ -0,0 +1,16 @@
+SELECT
+  p.name AS User_Name
+, dp.permission_name
+, dp.state_desc AS Permission_State
+, c.name AS ColumnName
+, OBJECT_NAME(o.object_id) AS TabName
+FROM sys.database_permissions dp
+INNER JOIN Sys.objects O
+     ON dp.major_id = o.object_id
+INNER JOIN sys.columns C
+     ON c.object_id = O.object_id
+        AND c.column_id = dp.minor_id
+INNER JOIN sys.database_principals P
+     ON p.principal_id = dp.grantee_principal_id
+--WHERE p.Name = 'G-FHLBAPPS-AHP Full'
+order by TabName, C.Name 
--- Get recent queries ran on the server.sql
+++ Get recent queries ran on the server.sql
@@ -1,5 +1,7 @@
 SELECT db_name(dest.dbid) as DBName,deqs.last_execution_time AS [Time],  dest.text AS [Query], dest.*
 FROM sys.dm_exec_query_stats AS deqs
 CROSS APPLY sys.dm_exec_sql_text(deqs.sql_handle) AS dest
---WHERE db_name(dest.dbid) LIKE '%HUB%'
-ORDER BY DBName DESC, Time desc
+--WHERE db_name(dest.dbid) LIKE '%On%'
+ORDER BY DBName DESC, Time DESC
+
+
