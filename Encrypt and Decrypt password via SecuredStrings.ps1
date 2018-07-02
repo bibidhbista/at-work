e2651a19540aba013bc77509c809c8174f179fa1
Bibidh Bista <BibidhB@a-t-g.com>
2018-07-02 09:00:09 -0500

    Weekly Commit_07022018

---
+++ Encrypt and Decrypt password via SecuredStrings.ps1
@@ -0,0 +1,7 @@
+﻿#$credential = Get-Credential
+#$credential.Password | ConvertFrom-SecureString | Set-Content c:\encrypted_password.txt
+
+$encrypted = Get-Content c:\encrypted_password.txt | ConvertTo-SecureString
+$credential = New-Object System.Management.Automation.PsCredential('atg\bibidhb', $encrypted)
+
+start-job {Invoke-Sqlcmd -ServerInstance bbista -Database master -Query "select @@version"} -Credential $credential|Wait-Job|Receive-Job
