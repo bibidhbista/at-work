f1ddf0e77b2aefc80b108d99b39969c0e2ad14a4
Bibidh Bista <BibidhB@a-t-g.com>
2018-07-06 09:00:06 -0500

    Weekly Commit_07062018

---
+++ Copy Hash Check/Copy-Hash Check.ps1
@@ -0,0 +1,22 @@
+﻿$file =  "C:\Users\bibidhb\Desktop\WO\*"
+$dest = "E:\BackupDemo"
+ 
+dir $file -Recurse| foreach {
+     #calculate current hash
+     Write-Host "Copying $($_.fullname) to $dest" -ForegroundColor Yellow
+     $origHash = Get-FileHash -Path $_.FullName -Algorithm MD5
+ 
+     #copy file
+     $filecopy = $_ | Copy-Item -Destination $dest -PassThru
+ 
+     #get hash of copied file
+     $copyHash = Get-FileHash -Path $filecopy.FullName -Algorithm MD5
+ 
+     #compare them
+     if ($origHash.hash -ne $copyHash.hash) {
+            Write-Warning "$($_.Fullname) and $($filecopy.fullname) hash mismatch" 
+     }
+     else {
+            Write-Host "$($_.Fullname) and $($filecopy.fullname) hash ok" -ForegroundColor Green
+     }
+} #foreach
