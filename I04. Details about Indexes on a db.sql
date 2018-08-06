ad3df00a7d3bd08bfb03b7fee73ca1b7e82af1ee
Bibidh Bista <BibidhB@a-t-g.com>
2018-08-06 09:00:04 -0500

    Weekly Commit_08062018

--- I04. Details about Indexes on a db.sql
+++ I04. Details about Indexes on a db.sql
@@ -1,41 +1,41 @@
---SELECT 
---     TableName = t.name,
---     IndexName = ind.name,
---	  ColumnName = col.name,
---	 ind.fill_factor,
---     IndexId = ind.index_id,
---     ColumnId = ic.index_column_id,
---     ind.*,
---     ic.*,
---     col.* 
---FROM 
---     sys.indexes ind 
---INNER JOIN 
---     sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id 
---INNER JOIN 
---     sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
---INNER JOIN 
---     sys.tables t ON ind.object_id = t.object_id 
---WHERE 
---     ind.is_primary_key = 0 
---     AND ind.is_unique = 0 
---     AND ind.is_unique_constraint = 0 
---     AND t.is_ms_shipped = 0 
---	 AND (IND.NAME like '%locationcd%' or ind.name like '%offendercd%')
---	--and ind.fill_factor != '100'
---	 --and t.name = 'aPsaBondReviewHearings' 
---ORDER BY 
---     t.name, ind.name, ind.index_id, ic.index_column_id;
+SELECT 
+     TableName = t.name,
+     IndexName = ind.name,
+	  ColumnName = col.name,
+	 ind.fill_factor,
+     IndexId = ind.index_id,
+     ColumnId = ic.index_column_id,
+     ind.*,
+     ic.*,
+     col.* 
+FROM 
+     sys.indexes ind 
+INNER JOIN 
+     sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id 
+INNER JOIN 
+     sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
+INNER JOIN 
+     sys.tables t ON ind.object_id = t.object_id 
+WHERE 
+     ind.is_primary_key = 0 
+     AND ind.is_unique = 0 
+     AND ind.is_unique_constraint = 0 
+     AND t.is_ms_shipped = 0 
+	 AND (IND.NAME like '%locationcd%' or ind.name like '%offendercd%')
+	--and ind.fill_factor != '100'
+	 --and t.name = 'aPsaBondReviewHearings' 
+ORDER BY 
+     t.name, ind.name, ind.index_id, ic.index_column_id;
 
 
 
 
 
--- SELECT COLUMN_NAME,*
---FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
---WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + QUOTENAME(CONSTRAINT_NAME)), 'IsPrimaryKey') = 1
---AND TABLE_NAME like '%victim%' and table_name not like 'H_*'--AND TABLE_SCHEMA = 'Schema'
---and COLUMN_NAME like '%locationcd%'
+ SELECT COLUMN_NAME,*
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + QUOTENAME(CONSTRAINT_NAME)), 'IsPrimaryKey') = 1
+AND TABLE_NAME like '%victim%' and table_name not like 'H_*'--AND TABLE_SCHEMA = 'Schema'
+and COLUMN_NAME like '%locationcd%'
 
  
 
@@ -66,7 +66,7 @@ inner join syscolumns c on
 inner join sys.tables d on 
        a.id = d.object_id
        and d.is_ms_shipped <> 1
-where a.indid=1 and (c.name like '%cd%')
+where a.indid=1 --and (c.name like '%locationcd%' or c.name like '%offendercd%')
 group by a.name
 having count(*) > 1
 ) 
