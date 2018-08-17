2a464180af3fd23c21719aee1f30c7bb58d5d005
Bibidh Bista <BibidhB@a-t-g.com>
2018-08-17 09:00:05 -0500

    Weekly Commit_08172018

---
+++ ATG/02. Add dev Access to Users.ps1
@@ -0,0 +1,22 @@
+﻿$dbName='DOCSecurity_Test'
+$userName='louisM'
+
+
+
+
+
+
+
+$server = 'atgdsmsq14'
+$query = @"
+use [$dbName]
+
+create user [atg\$userName] for login [atg\$userName] with default_schema = [atg\$userName];
+alter role devteam add member [atg\$userName];
+
+"@
+
+
+
+
+Invoke-Sqlcmd -ServerInstance $server -Database $dbName -Query $query
