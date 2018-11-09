aa3a953af6ba20e3fee9f2dde39d9f612d5d4e20
bbista <BibidhB@a-t-g.com>
2018-11-09 09:00:02 -0600

    Weekly Commit_11092018

---
+++ PowerShell/Retrieve Password from RDG file.ps1
@@ -0,0 +1,5 @@
+﻿$PwdString = '';
+Copy-Item 'C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe' 'C:\temp\RDCMan.dll' 
+Import-Module 'C:\temp\RDCMan.dll'
+$EncryptionSettings = New-Object -TypeName RdcMan.EncryptionSettings
+[RdcMan.Encryption]::DecryptString($PwdString, $EncryptionSettings)
