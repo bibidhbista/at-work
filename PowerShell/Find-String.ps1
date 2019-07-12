0bf0d07c8b5dccf1f17b09ab239bec5104fee93f
bbista <BibidhB@a-t-g.com>
2019-07-12 09:00:03 -0500

    Weekly Commit_07122019

---
+++ PowerShell/Find-String.ps1
@@ -0,0 +1,17 @@
+﻿#VARS
+$SearchString ='s_InmateInfo_GetInmatesForCentricUpload'
+
+
+$path = 'C:\Projects\DBA\ATG_YIT\ATG_InmateKiosk\Database\Rollout\Update'
+
+
+
+$oPath = 'C:\Users\bibidhb\Desktop\Ad hoc\StringFind'
+if (!(Test-Path $oPath)){md $oPath}
+$Outputfile ="$oPath\Results.sql"
+
+# MAIN
+cd $path
+$results = findstr /s /i /c:"$SearchString" *.sql
+$results|Out-File $Outputfile
+ii $Outputfile
